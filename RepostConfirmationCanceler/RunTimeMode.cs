04f9250413356b9bdc2335ec284facf878fd5503
Takashi Hashida <hashida@clear-code.com>
2025-09-08 13:48:28 +0900

    Add MPL License text to RepostConfirmationCanceler

--- RepostConfirmationCanceler/RunTimeMode.cs
+++ RepostConfirmationCanceler/RunTimeMode.cs
@@ -1,4 +1,11 @@
-﻿namespace RepostConfirmationCanceler
+﻿/*
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+Copyright (c) 2025 ClearCode Inc.
+*/
+namespace RepostConfirmationCanceler
 {
     internal enum RunTimeMode
     {
